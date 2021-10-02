fed6fdcdd1b2cffaa2a7e28d5407e190263cf8e0
Juan Romero <juan@finstarter.co>
2021-10-02 01:39:38 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    'sumary': """Academy app to manage Training""",
+    'description': """Academy Module to manage Training:
+    -Courses
+    -Sessions
+    -Attendees""",
+    'author': 'Juan',
+    'website': 'https://www.odoo.com',
+    'category': 'Training',
+    'version': '0.1',
+    'depends':['base'],
+    'data':[],
+    'demo':[],
+}
