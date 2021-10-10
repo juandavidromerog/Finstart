d812ecc72db23ce73414e83763ff0d7dbc036ba7
Your Name <juan@finstarter.co>
2021-10-10 23:09:06 +0000

    [ADD] Basic Module

---
+++ real_estate/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Juan",
+    'category': 'Real Estate',
+    'description': """
+    Modulo para manejar bienes raices
+    """,
+    # data files always loaded at installation
+    'data': [
+        
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        
+    ]
+}
